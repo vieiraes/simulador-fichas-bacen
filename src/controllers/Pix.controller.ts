ebebcf96dc5d214609903abb5f3c84eafdb284b1
Bruno Vieira <vieira.es@gmail.com>
2023-10-19 10:43:06 -0300

    fix controller

--- src/controllers/Pix.controller.ts
+++ src/controllers/Pix.controller.ts
@@ -1,12 +1,12 @@
-import { Request, Response } from "express";
+import { Request, Response } from "express"
 import express from 'express'
-require('dotenv').config();
+require('dotenv').config()
 
 const router = express.Router()
 
-const wallet1 = { id: "469834d9-61b0-4893-81c1-94418f300e0a", balde: 0 }
-const wallet2 = { id: "d2dde215-98d8-43be-954f-384ed8c3de4c", balde: 0 }
-const wallet3 = { id: "cf82633d-71f9-4ff0-ab3e-7a8163d3fc06", balde: 0 }
+const wallet1 = { id: "469834d9-61b0-4893-81c1-94418f300e0a", balde: Number(process.env.MAX_TOKENS) }
+const wallet2 = { id: "d2dde215-98d8-43be-954f-384ed8c3de4c", balde: Number(process.env.MAX_TOKENS) }
+const wallet3 = { id: "cf82633d-71f9-4ff0-ab3e-7a8163d3fc06", balde: Number(process.env.MAX_TOKENS) }
 export let wallets = [wallet1, wallet2, wallet3]
 
 //AXIOS
@@ -17,23 +17,27 @@ router.post('/balde', (req: Request, res: Response) => {
 
     if (walletReturn.balde < Number(process.env.MAX_TOKENS)) {
         walletReturn.balde += 2
-        res.status(201).json({ walletReturn, message: "Fichas recuperadas", "BACEN": "ENTRIES_READ_USER_ANTISCAN_V2" })
+        res.status(201).json({
+            message: "2 fichas recuperadas no balde",
+            walletReturn
+        })
     }
     if (walletReturn.balde == Number(process.env.MAX_TOKENS)) {
-        res.status(200).json({ walletReturn, message: "Limite atingido", "BACEN": "ENTRIES_READ_USER_ANTISCAN_V2" })
+        res.status(200).json({
+            message: "Limite do balde atingido",
+            "BACEN": "ENTRIES_READ_USER_ANTISCAN_V2",
+            walletReturn
+        })
     }
     if (!walletReturn) {
-        res.status(404).json({ message: "Wallet não encontrado" })
+        res.status(404).json({ message: "Wallet não encontrada", walletReturn })
     }
 })
 
 
 router.get('/balde', (req: Request, res: Response) => {
-
     res.status(200).json(wallets)
-
 })
-
 
 
 router.post('/success', (req: Request, res: Response) => {
@@ -42,19 +46,18 @@ router.post('/success', (req: Request, res: Response) => {
     const walletReturn = wallets.find(w => w.id === wallet)
 
     if (walletReturn.balde < 21) {
-        res.status(400).json({ "message": 'Pix não permitido, saldo do balde insuficiente', walletReturn })
+        res.status(400).json({ "message": 'Pix não permitido. Saldo do balde insuficiente', walletReturn })
     }
     if (walletReturn.balde > 20) {
         walletReturn.balde -= 1
-        res.status(201).json({ "message": 'Pix efetuado com sucesso', walletReturn })
+        res.status(201).json({ "message": 'Pix efetuado com sucesso.', walletReturn })
         walletReturn.balde += 1
     }
 
     if (!walletReturn) {
-        res.status(404).json({ message: "Wallet não encontrado" })
+        res.status(404).json({ message: "Wallet não encontrada", walletReturn })
     }
 })
-
 
 
 router.post('/fail', (req: Request, res: Response) => {
@@ -63,17 +66,20 @@ router.post('/fail', (req: Request, res: Response) => {
     const walletReturn = wallets.find(w => w.id === wallet)
 
     if (walletReturn.balde < 20) {
-        res.status(400).json({ "message": 'Pix não permitido, saldo no balde insuficiente', walletReturn })
+        res.status(429).json({
+            "message": 'Pix não permitido. Saldo do balde insuficiente',
+            "BACEN": "ENTRIES_READ_USER_ANTISCAN_V2",
+            walletReturn
+        })
     }
     if (walletReturn.balde >= 20) {
         walletReturn.balde -= 20
-        res.status(400).json({ "message": 'Pix nao efeutado. falha na consulta de chave', walletReturn })
+        res.status(400).json({ "message": 'Pix não efeutado. Chave inválida.', walletReturn })
     }
     if (!walletReturn) {
-        res.status(404).json({ message: "Wallet não encontrado" })
+        res.status(404).json({ message: "Wallet não encontrada." })
     }
 })
-
 
 
 export { router }
--- src/cron.ts
+++ src/cron.ts
@@ -4,23 +4,18 @@ import { wallets } from './controllers/Pix.controller'
 
 async function execute(walletid) {
     try {
-        const response = await axios.post(`http://localhost:3000/pix/balde?wallet=${walletid}`);
-        console.log(response.data);
+        const response = await axios.post(`http://localhost:3000/pix/balde?wallet=${walletid}`)
+        console.log(response.data)
+        
     } catch (error) {
-        console.error(error);
+        console.error(error)
     }
 }
 
-
-const job = new CronJob('*/2 * * * * *', () => {
-
-    wallets.forEach(w => {
-        execute(w.id)
-    })
-
+const job = new CronJob('*/2 * * * * *', async () => {
+    const promises = wallets.map(w => execute(w.id))
+    await Promise.all(promises)
     console.log('cron running a task every 2 seconds')
-},
-    null, true, 'America/Sao_Paulo'
-)
+}, null, true, 'America/Sao_Paulo')
 
 job.start()
